7c6b300b9ce4d3c9660f2fb35c28b1be12e50b37
kburke515 <kellyburke@Socials-MacBook-Air-91.local>
2014-06-18 20:22:18 -0400

    default text

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.get('/', function(req, res){
 	console.log("req params", req.query);
   res.render('index.ejs',
       {
-            top: req.query.top,
-            bottom: req.query.bottom
+            top: req.query.top || "Get Your",
+            bottom: req.query.bottom || "Meme On"
       });
 });
 
